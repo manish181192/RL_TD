c77b4bf14505dd30871584e4bfaea5a404ce24fb
manish181192 <manishvidyasagar@gmail.com>
2017-02-23 19:14:17 +0530

    Bug fix
    Bug fix

--- TD_lambda_Prediction_fw_bw_view.py
+++ TD_lambda_Prediction_fw_bw_view.py
@@ -22,19 +22,21 @@ def compute_discounted_reward(discount, reward, start_time_step, end_time_step):
     time_diff = end_time_step - start_time_step
     g = np.zeros(time_diff + 1)
     i = end_time_step
+    j = time_diff
     prev_g = 0
     while i >= start_time_step:
-        g[i] = reward[i] + discount * prev_g
-        prev_g = g[i]
+        g[j] = reward[i] + discount * prev_g
+        prev_g = g[j]
         i = i - 1
+        j = j - 1
     return g[0]
 #################################################
 
 ### TD-Lambda ###
 no_of_episodes = 100000
 TIME_STEP_LIMIT = 50
-discount = 0.6
-alpha = 0.001
+discount = 0.9
+alpha = 0.02
 td_N = 0
 state_value = defaultdict(float)
 start_time = time()
@@ -53,40 +55,41 @@ for i in range(no_of_episodes):
         print("State:" + str(state_list[time_step]) + " Action:" + str(action[time_step]) + " REWARD:" + str(reward[time_step]))
         ##########################################################
         if time_step > td_N:
-            i = time_step- td_N-1  # state for which value is updated
-            discounted_reward = compute_discounted_reward(discount, reward,i,time_step)
+            target_state = time_step- td_N-1  # state for which value is updated
+            discounted_reward = compute_discounted_reward(discount, reward,target_state,time_step)
             for j in range(td_N+1):
-                discounted_reward = discounted_reward + lambda_ * pow(discount, j + 1) * reward[j + 1]
-            td_error = (discounted_reward - state_value[state_list[i]])
-            state_value[state_list[i]] = state_value[state_list[i]] + alpha * td_error
+                discounted_reward = discounted_reward +  pow(lambda_ , j) * compute_discounted_reward(discount, reward, j+1, time_step)
+            lambda_discounted_reward = (1-lambda_) * discounted_reward
+            td_error = (discounted_reward - state_value[state_list[target_state]])
+            state_value[state_list[target_state]] = state_value[state_list[target_state]] + alpha * td_error
 
         ##########################################################
         if isTerminate:
             # computed updates for last td_N+1 states of episode
-
             # compute G(t) for each time step ##############
             # G(t) = Rt + d*G(t+1)
             g = np.zeros(td_N + 1)
-            i = time_step
+            current_step = time_step
             prev_g = 0
-            j=0
-            while i >= time_step-td_N:
-                g[j] = reward[i] + discount * prev_g
-                prev_g = g[j]
-                i = i - 1
-                j= j+1
+            reward_index = 0
+            while current_step >= time_step - td_N:
+                g[reward_index] = reward[current_step] + discount * prev_g
+                prev_g = g[reward_index]
+                current_step = current_step - 1
+                reward_index = reward_index + 1
             #################################################
 
             # calculate v(s) for every state ################
-            i = time_step-td_N
-            j=0
-            while i < time_step + 1:
+            ts = time_step - td_N
+            reward_index = 0
+            while ts < time_step + 1:
                 # if state_list[i] in state_value:
                 # current state has already occured, incremental mean
-                mc_error = (g[j] - state_value[state_list[i]])
-                state_value[state_list[i]] = state_value[state_list[i]] + alpha * mc_error
-                i = i+1
-                j=j+1
+                td_error = (g[reward_index] - state_value[state_list[ts]])
+                new_mean = state_value[state_list[ts]] + alpha * td_error
+                state_value[state_list[ts]] = new_mean
+                ts = ts + 1
+                reward_index = reward_index + 1
             break
         else:
             state_list.append(state_)
